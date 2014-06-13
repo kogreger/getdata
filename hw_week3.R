84be5462024bfb605a5201a6476fada9b008aaf4
Konstantin Greger <kogreger@gmail.com>
2014-06-13 18:58:55 +0900

    modified R code file for thrid week's homework

--- hw_week3.R
+++ hw_week3.R
@@ -6,6 +6,7 @@ agricultureLogical <- ifelse(acs$ACR == 3 & acs$AGS == 6, TRUE, FALSE)
 sum(agricultureLogical, na.rm = TRUE)
 head(which(agricultureLogical), 3)
 
+
 ## Q2
 #install.packages("jpeg")
 library(jpeg)
@@ -14,15 +15,44 @@ download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg",
 jeffJpeg <- readJPEG("getdata_jeff.jpeg", native = TRUE)
 quantile(jeffJpeg, c(0.3, 0.8))
 
+
 ## Q3
+# load gdp dataset
 download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv", 
               destfile = "gdp.csv")
 gdp <- read.csv("gdp.csv", skip = 5, header = F)
+#remove unnecessary columns
 keep = c(1, 2, 4, 5)
 gdp <- gdp[keep]
+# provide meaningful column names
 names(gdp) <- c("code", "rank", "name", "gdp")
+# remove rows with non-numeric rank (remove factor first)
+gdp$rank <- as.character(gdp$rank)
+gdp <- gdp[grep("^([0-9]+)$", gdp$rank), ]
+gdp$rank <- as.numeric(gdp$rank)
+
+# load education dataset
 download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", 
               destfile = "education.csv")
 edu <- read.csv("education.csv")
+
+#merge two data frames
 df <- merge(gdp, edu, by.x = "code", by.y = "CountryCode")
-# not done yet
+nrow(df)
+
+#sort resulting data frame
+library(plyr)
+df <- arrange(df, desc(rank))
+df[13, c("rank", "name")]
+tail(df, 2)
+
+
+## Q4
+tapply(df$rank, df$Income.Group, mean)
+
+
+## Q5
+#install.packages("Hmisc")
+library(Hmisc)
+df$group <- cut2(df$rank, g = 5)
+xtabs( ~ group + Income.Group, data = df)
